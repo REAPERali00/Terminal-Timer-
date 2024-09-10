86e7a0feef90da6276f25a6edda2d981455f0c5a
alireza <alireza2000jadidi@gmail.com>
2024-09-10 11:07:37 -0400

    initial commit

---
+++ time.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "No time found. please enter the duration for timer in Minuets "
+  exit 1
+fi
+
+min=$1
+
+sec=$((min * 60))
+while [ $sec -gt 0 ]; do
+  echo -ne "Time Left: $(date -u --date @$sec +%H:%M:%S)\r"
+  sleep 1
+  ((sec--))
+done
+
+echo -n "Time is up!!"
+mpv ./timer.mp3 >/dev/null 2>&1
